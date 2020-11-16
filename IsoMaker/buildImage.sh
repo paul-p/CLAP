d60f2436e06bcd7dcede2a5890995b1d30451fb3
Paul <perie@springeo.fr>
2020-11-16 22:42:17 +0100

    [Feature] Improve the first version

--- IsoMaker/buildImage.sh
+++ IsoMaker/buildImage.sh
@@ -3,6 +3,29 @@
 # Doc of Docker in Docker: https://devopscube.com/run-docker-in-docker/
 # Doc of Custom ISO with Docker: https://slai.github.io/posts/customising-ubuntu-live-isos-with-docker/
 
+# Select the right image type
+question='Choisissez le type de poste: '
+archs=("Postes Invités" "Poste Administrateur")
+select arch in "${archs[@]}"; do
+    case $arch in
+        "Postes Invités")
+            DOCKER_FILE_PATH='/data/Dockerfile_guest'
+            echo "Génération de l'image des postes invités"
+            break
+            ;;
+        "Poste Administrateur")
+            DOCKER_FILE_PATH='/data/Dockerfile_admin'
+            echo "Génération de l'image du poste d'administration"
+	        break
+            ;;
+	"Quit")
+	    echo "Fin du programme"
+	    exit
+	    ;;
+        *) echo "Option invalide $REPLY";;
+    esac
+done
+
 # Select the right architecture
 question='Choisissez votre architecture: '
 archs=("64 bits / amd64" "32 bits / i386")
@@ -18,29 +41,6 @@ select arch in "${archs[@]}"; do
             UBUNTU_ISO_PATH='/data/downloads/xubuntu-18.04.5-desktop-i386.iso'
             echo "Selection de l'architecture 32 bits avec Xubuntu. Téléchargement en cours..."
             wget --directory-prefix ./data/downloads -c "http://cdimage.ubuntu.com/xubuntu/releases/18.04/release/xubuntu-18.04.5-desktop-i386.iso"
-	        break
-            ;;
-	"Quit")
-	    echo "Fin du programme"
-	    exit
-	    ;;
-        *) echo "Option invalide $REPLY";;
-    esac
-done
-
-# Select the right image type
-question='Choisissez le type de poste: '
-archs=("Postes Invités" "Poste Administrateur")
-select arch in "${archs[@]}"; do
-    case $arch in
-        "Postes Invités")
-            DOCKER_FILE_PATH='/data/Dockerfile_guest'
-            echo "Génération de l'image des postes invités"
-            break
-            ;;
-        "Poste Administrateur")
-            DOCKER_FILE_PATH='/data/Dockerfile_admin'
-            echo "Génération de l'image du poste d'administration"
 	        break
             ;;
 	"Quit")
