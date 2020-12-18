7cde94fda73ada9e2d7eb931056abcabb109ee03
Paul <mail.paul.perie@gmail.com>
2020-12-18 18:17:38 +0100

    Complete the awxServer and secretStorage roles

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,14 @@ sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -y ansible
 
+# Create group docker and add user to it
+ if grep -q docker /etc/group
+    then
+         echo "group docker already exists"
+    else
+         sudo groupadd docker
+    fi
+export userId=`whoami` && sudo usermod -a -G docker2 $userId && newgrp docker2
+
 # Run the AWX server
 ansible-playbook -K ansiblePlaybooks/Init_Administrator.yml
